8ab8a120445f102bb14ece908203ab55a8f06249
akitora <hayashi20030703@gmail.com>
2025-02-06 09:29:23 +0900

    テーマカラーを選ぶためのカラーパレットをSettingsPageに追加した。選んだ瞬間に変更する処理はこれから実装する

--- lib/view/settings_page/settings_page.dart
+++ lib/view/settings_page/settings_page.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:redux_practice/redux/store/rp_app_state_provider.dart';
+import 'package:redux_practice/resource/rp_theme/rp_theme_type.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends ConsumerWidget {
   const SettingsPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final rpTheme = CupertinoTheme.of(context);
+    final selectedThemeType = ref
+        .watch(rpAppStateProvider.select((state) => state.selectedThemeType));
+
     return CupertinoPageScaffold(
       navigationBar: _buildNavigationBar(context, rpTheme),
-      child: const Center(
-        child: Text('Setting Page'),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: _buildThemeSelection(context, ref, selectedThemeType),
+        ),
       ),
     );
   }
@@ -33,6 +42,70 @@ class SettingsPage extends StatelessWidget {
         child: const Icon(CupertinoIcons.back, size: 24),
         onPressed: () => Navigator.pop(context), // 戻るボタンの動作
       ),
+    );
+  }
+
+  // MARK: - Theme Selection
+  Widget _buildThemeSelection(
+    BuildContext context,
+    WidgetRef ref,
+    RPThemeType selectedThemeType,
+  ) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 16),
+        const Text(
+          "Theme colors",
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 16),
+        _buildColorPalette(context, ref, selectedThemeType),
+      ],
+    );
+  }
+
+  // MARK: - Color Palette
+  Widget _buildColorPalette(
+    BuildContext context,
+    WidgetRef ref,
+    RPThemeType selectedThemeType,
+  ) {
+    final brightness = MediaQuery.of(context).platformBrightness;
+    final isDarkMode = brightness == Brightness.dark;
+    const themeOptions = RPThemeType.values;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: themeOptions.map((themeType) {
+        final isSelected = themeType == selectedThemeType;
+        return CupertinoButton(
+          padding: EdgeInsets.zero,
+          onPressed: () {
+            // ref.read(rpAppStateProvider.notifier).updateTheme(themeType);
+          },
+          child: Container(
+            width: 40,
+            height: 40,
+            margin: const EdgeInsets.symmetric(horizontal: 8),
+            decoration: BoxDecoration(
+              color: CupertinoDynamicColor.resolve(
+                  themeType.light.primaryColor, context),
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: isSelected
+                    ? (isDarkMode
+                        ? CupertinoColors.white
+                        : CupertinoColors.systemGrey)
+                    : (isDarkMode
+                        ? CupertinoColors.systemGrey
+                        : CupertinoColors.systemGrey3),
+                width: isSelected ? 3 : 1.5,
+              ),
+            ),
+          ),
+        );
+      }).toList(),
     );
   }
 }
